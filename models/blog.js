d4fc6318d4c970a9796f2a7e5c610502f7bc49e2
heysungj <linjing0525@gmail.com>
2022-10-06 10:54:34 -0400

    add blog model

---
+++ models/blog.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+  title: { type: String, required: true },
+  date: {
+    type: Date,
+    required: true,
+  },
+  content: {
+    type: String,
+  },
+  photo: [String],
+  timestamps: true,
+  toJSON: {
+    transform: function (doc, ret) {
+      delete ret.password;
+      return ret;
+    },
+  },
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
