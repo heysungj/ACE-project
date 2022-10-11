629994de770102ed8225184c0f047c470fbcfb55
heysungj <linjing0525@gmail.com>
2022-10-11 14:08:26 -0400

    add multer for aws s3

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,20 @@ const router = express.Router();
 const usersCtrl = require("../../controllers/api/users");
 const ensureLoggedIn = require("../../config/ensureLoggedIn");
 
+// for aws to save photos
+const multer = require("multer");
+
+// setup multer middleware to parse form-data
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, req.user._id.toString() + "-" + file.originalname);
+  },
+});
+const upload = multer({ storage: storage, limits: { fieldSize: 5000 } });
+
 // POST /api/users
 router.post("/", usersCtrl.create);
 // POST /api/users/login
@@ -16,6 +30,6 @@ router.put("/blogs/:id", usersCtrl.editBlog);
 // DELETE /api/users/blogs/delete/:id
 router.get("/blogs/delete/:id", usersCtrl.deleteBlog);
 // GET /api/users/check-token
-router.get("/check-token", ensureLoggedIn, usersCtrl.checkToken);
+// router.get("/check-token", ensureLoggedIn, usersCtrl.checkToken);
 
 module.exports = router;
