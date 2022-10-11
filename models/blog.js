7ce29b8524f25c98b72eca6f6bc73ca01dfffb21
heysungj <linjing0525@gmail.com>
2022-10-10 23:52:40 -0400

    admin registered

--- models/blog.js
+++ models/blog.js
@@ -1,20 +1,26 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const blogSchema = new Schema({
-  title: { type: String, required: true },
-  content: {
-    type: String,
-  },
-  photo: [String],
-  video: String,
-  timestamps: true,
-  toJSON: {
-    transform: function (doc, ret) {
-      delete ret.password;
-      return ret;
+const blogSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    content: {
+      type: String,
+    },
+    photo: [String],
+    video: {
+      type: String,
     },
   },
-});
+  {
+    timestamps: true,
+    toJSON: {
+      transform: function (doc, ret) {
+        delete ret.password;
+        return ret;
+      },
+    },
+  }
+);
 
 module.exports = mongoose.model("Blog", blogSchema);
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import LoginForm from "../../components/LoginForm/LoginForm";
 
 export default function AuthPage({ setUser }) {
@@ -9,6 +9,7 @@ export default function AuthPage({ setUser }) {
       <h1>AuthPage</h1>
 
       <LoginForm setUser={setUser} />
+      <SignUpForm setUser={setUser} />
     </main>
   );
 }
