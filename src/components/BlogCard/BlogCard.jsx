8f076cd404097b8e49322fff8b11bbedd79b2985
heysungj <linjing0525@gmail.com>
2022-10-18 09:32:58 -0400

    switch to contentful

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,89 +1,23 @@
 import "./BlogCard.css";
+import marked from "marked";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { AiOutlineEdit } from "react-icons/ai";
-import Modal from "react-modal";
-import EditBlog from "../EditBlog/EditBlog";
 
-export default function ProductCard({
-  title,
-  photo,
-  content,
-  date,
-  user,
-  blog,
-  blogList,
-  setBlogList,
-}) {
+export default function ProductCard({ article, user }) {
+  const { title, content, date, photo } = article.fields;
   // use navigate
   const navigate = useNavigate();
-  const customStyles = {
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-    },
-  };
-
-  // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
-  // Modal.setAppElement("#root");
-
-  let subtitle;
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = "#003580";
-  }
-
-  function closeModal() {
-    setIsOpen(false);
-  }
 
   ///////////////////////////////////////////
 
   return (
     <div className="eachProductContainer">
       <div className="eachProduct">
-        <h4 className="className">{title}</h4>
-        <p>{date}</p>
-        {/* {user ? ( */}
-        <AiOutlineEdit
-          onClick={openModal}
-          size={"2rem"}
-          style={{ cursor: "pointer", margin: "0.5rem", color: "purple" }}
-        />
-        {/* ) : null} */}
+        <h3>{title}</h3>
+        <h5>{date}</h5>
+        <p>{content}</p>
       </div>
-      {photo.map((img, index) => {
-        return <img className="blogImg" src={img} alt="" />;
-      })}
-
-      <p>{content}</p>
-
-      {/* modal */}
-      <Modal
-        isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >
-        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Edit Blog</h2>
-        <EditBlog
-          setBlogList={setBlogList}
-          blog={blog}
-          blogList={blogList}
-          closeModal={closeModal}
-        />
-      </Modal>
     </div>
   );
 }
--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import Modal from "react-modal";
-import AddBlog from "../../components/AddBlog/AddBlog";
 import BlogCard from "../../components/BlogCard/BlogCard";
-import * as blogAPI from "../../utilities/users-api";
 import { useEffect, useState } from "react";
 import * as contentful from "contentful";
 
 export default function Blogs({ user }) {
-  const [blogList, setBlogList] = useState([]);
   const [articles, setArticles] = useState([]);
 
   // contenful set up
@@ -15,52 +11,20 @@ export default function Blogs({ user }) {
     space: process.env.REACT_APP_SPACE_ID,
     accessToken: process.env.REACT_APP_CONTENTFUL_API,
   });
-  // set up react-modal
-  const customStyles = {
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-    },
-  };
-
-  // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
-  Modal.setAppElement("#root");
-
-  let subtitle;
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = "#003580";
-  }
-
-  function closeModal() {
-    setIsOpen(false);
-  }
 
   ///////////////////////////////////////////'
 
   useEffect(() => {
     const blogs = async () => {
-      const allBlogs = await blogAPI.getBlog();
-      setBlogList(allBlogs);
-      console.log("allBlogs", allBlogs);
-
-      // fetch contentful api call
       client
         .getEntries()
         .then((response) => {
           console.log(response);
+          setArticles(response.items);
         })
         .catch(console.error);
+
+      console.log("articles", articles);
     };
 
     blogs();
@@ -68,31 +32,9 @@ export default function Blogs({ user }) {
   return (
     <main>
       <h1>Blogs page</h1>
-      <button onClick={openModal}>Add Blog</button>
-      <Modal
-        isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >
-        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Add New Blog</h2>
-        <AddBlog closeModal={closeModal} blogList={blogList} user={user} />
-      </Modal>
 
-      {blogList.map((blog, index) => {
-        return (
-          <BlogCard
-            title={blog.title}
-            photo={blog.photo}
-            content={blog.content}
-            date={blog.createdAt}
-            blogList={blogList}
-            blog={blog}
-            setBlogList={setBlogList}
-            user={user}
-          />
-        );
+      {articles.map((article, index) => {
+        return <BlogCard key={index} article={article} user={user} />;
       })}
     </main>
   );
