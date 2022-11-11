f620c0dd047cf8f869bcc309d218e877c9c52e96
heysungj <linjing0525@gmail.com>
2022-11-10 21:36:26 -0500

    contact form width change

--- src/pages/ContactForm/ContactForm.jsx
+++ src/pages/ContactForm/ContactForm.jsx
@@ -72,7 +72,7 @@ export default function ContactForm() {
       class="cotainer border contactFormContainer"
       style={{
         marginTop: "50px",
-        width: "80%",
+        width: "90%",
         backgroundColor: "bisque",
         backgroundPosition: "center",
         backgroundSize: "cover",
